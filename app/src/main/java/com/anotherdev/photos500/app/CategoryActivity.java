4311890608bc17884f2781300da438c1f309a5d8
anotherdev <polson.k+github@gmail.com>
2016-06-11 14:14:59 +0700

    add app & base activity

--- app/src/main/java/com/anotherdev/photos500/app/CategoryActivity.java
+++ app/src/main/java/com/anotherdev/photos500/app/CategoryActivity.java
@@ -1,11 +1,10 @@
 package com.anotherdev.photos500.app;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.anotherdev.photos500.R;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends P5Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/anotherdev/photos500/app/P5App.java
@@ -0,0 +1,11 @@
+package com.anotherdev.photos500.app;
+
+import android.app.Application;
+
+public class P5App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
