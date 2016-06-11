1a344eca467e64b4461c5c4bf71a014a4bd56cb5
anotherdev <polson.k+github@gmail.com>
2016-06-11 13:12:35 +0700

    initial commit

---
+++ app/src/main/java/com/anotherdev/photos500/app/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.anotherdev.photos500.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anotherdev.photos500.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
