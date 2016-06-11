6653a8ca0da752e6f30aaf5c803df3de47afd3a0
anotherdev <polson.k+github@gmail.com>
2016-06-11 18:11:47 +0700

    add crashlytics

--- app/src/main/java/com/anotherdev/photos500/app/P5App.java
+++ app/src/main/java/com/anotherdev/photos500/app/P5App.java
@@ -2,10 +2,30 @@ package com.anotherdev.photos500.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+
 public class P5App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initFabric();
+    }
+
+    private void initFabric() {
+        Crashlytics crashlytics = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder()
+                        .build())
+                .build();
+
+        Fabric fabric = new Fabric.Builder(this)
+                .kits(crashlytics)
+                .debuggable(true)
+                .build();
+
+        Fabric.with(fabric);
     }
 }
