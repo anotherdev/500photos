98f98f82ae833c6a6f8398765533fe604f8c5e91
anotherdev <polson.k+github@gmail.com>
2016-06-13 00:03:35 +0700

    add key for photo page

--- app/src/main/java/com/anotherdev/photos500/api/dto/PhotoPage.java
+++ app/src/main/java/com/anotherdev/photos500/api/dto/PhotoPage.java
@@ -1,17 +1,23 @@
 package com.anotherdev.photos500.api.dto;
 
 import com.google.gson.annotations.SerializedName;
+import com.karumi.rosie.repository.datasource.Identifiable;
 
 import java.util.Collections;
 import java.util.List;
 
-public class PhotoPage {
+public class PhotoPage implements Identifiable<Long> {
 
     @SerializedName("current_page") long currentPage;
     @SerializedName("total_pages") long totalPages;
     @SerializedName("total_items") long totalItems;
     @SerializedName("photos") List<Photo> photos = Collections.emptyList();
 
+
+    @Override
+    public Long getKey() {
+        return getCurrentPage();
+    }
 
     public long getCurrentPage() {
         return currentPage;
